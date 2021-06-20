ff0866e3c82fdac5492de0a3625fa64b6564b169
Kato Borghijs <kato_borghijs@icloud.com>
2021-06-20 15:35:03 +0200

    Removed duplicate data

--- src/script.js
+++ src/script.js
@@ -52,13 +52,13 @@ function displayForecast(response) {
         forecastHTML +
         `
       <div class="row forecast">
-        <div class="col-3">
+        <div class="col-4">
           <p>${formatDay(forecastDay.dt)}</p>
         </div>
-        <div class="col-3">
+        <div class="col-4">
           <p class="temperature">${Math.round(forecastDay.temp.day)}˚C</p>
         </div>
-        <div class="col-6">
+        <div class="col-4">
           <img src="images/${
             forecastDay.weather[0].icon
           }.png" alt="sunny" class="forcast-image" />
@@ -67,118 +67,6 @@ function displayForecast(response) {
     }
   });
 
-  forecastElement.innerHTML = forecastHTML;
-}
-
-// forecast function current location
-function displayForecastCurrentLocation(response) {
-  let forecast = response.data.daily;
-  let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = "";
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-      <div class="row forecast">
-        <div class="col-3">
-          <p>${formatDay(forecastDay.dt)}</p>
-        </div>
-        <div class="col-3">
-          <p class="temperature">${Math.round(forecastDay.temp.day)}˚C</p>
-        </div>
-        <div class="col-6">
-          <img src="images/${
-            forecastDay.weather[0].icon
-          }.png" alt="sunny" class="forcast-image" />
-        </div>
-      </div>`;
-    }
-  });
-  forecastElement.innerHTML = forecastHTML;
-}
-
-// forecast function Berlin
-function displayForecastBerlin(response) {
-  let forecast = response.data.daily;
-  let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = "";
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-      <div class="row forecast">
-        <div class="col-3">
-          <p>${formatDay(forecastDay.dt)}</p>
-        </div>
-        <div class="col-3">
-          <p class="temperature">${Math.round(forecastDay.temp.day)}˚C</p>
-        </div>
-        <div class="col-6">
-          <img src="images/${
-            forecastDay.weather[0].icon
-          }.png" alt="sunny" class="forcast-image" />
-        </div>
-      </div>`;
-    }
-  });
-  forecastElement.innerHTML = forecastHTML;
-}
-
-// forecast function Luxemburg
-function displayForecastLuxemburg(response) {
-  let forecast = response.data.daily;
-  let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = "";
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-      <div class="row forecast">
-        <div class="col-3">
-          <p>${formatDay(forecastDay.dt)}</p>
-        </div>
-        <div class="col-3">
-          <p class="temperature">${Math.round(forecastDay.temp.day)}˚C</p>
-        </div>
-        <div class="col-6">
-          <img src="images/${
-            forecastDay.weather[0].icon
-          }.png" alt="sunny" class="forcast-image" />
-        </div>
-      </div>`;
-    }
-  });
-  forecastElement.innerHTML = forecastHTML;
-}
-
-// forecast function Paris
-function displayForecastParis(response) {
-  let forecast = response.data.daily;
-  let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = "";
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-      <div class="row forecast">
-        <div class="col-3">
-          <p>${formatDay(forecastDay.dt)}</p>
-        </div>
-        <div class="col-3">
-          <p class="temperature">${Math.round(forecastDay.temp.day)}˚C</p>
-        </div>
-        <div class="col-6">
-          <img src="images/${
-            forecastDay.weather[0].icon
-          }.png" alt="sunny" class="forcast-image" />
-        </div>
-      </div>`;
-    }
-  });
   forecastElement.innerHTML = forecastHTML;
 }
 
@@ -191,49 +79,39 @@ function getForecast(coordinates) {
   axios.get(apiUrl).then(displayForecast);
 }
 
-// get coordinates for forecast function current location
+function displayForecastCurrentLocation(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = "";
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="row forecast">
+        <div class="col-4">
+          <p>${formatDay(forecastDay.dt)}</p>
+        </div>
+        <div class="col-4">
+          <p class="temperature">${Math.round(forecastDay.temp.day)}˚C</p>
+        </div>
+        <div class="col-4">
+          <img src="images/${
+            forecastDay.weather[0].icon
+          }.png" alt="sunny" class="forcast-image" />
+        </div>
+      </div>`;
+    }
+  });
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function getForecastCurrentLocation(coordinates) {
   console.log(coordinates);
   let apiKey = "39c5b362f47e0665da2f1eefc0c7ab6b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecastCurrentLocation);
-}
-
-// get coordinates for forecast function Berlin
-function getForecastBerlin(coordinates) {
-  console.log(coordinates);
-  let apiKey = "39c5b362f47e0665da2f1eefc0c7ab6b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
-  axios.get(apiUrl).then(displayForecastBerlin);
-}
-
-// get coordinates for forecast function Brussels
-function getForecastBrussels(coordinates) {
-  console.log(coordinates);
-  let apiKey = "39c5b362f47e0665da2f1eefc0c7ab6b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
-  axios.get(apiUrl).then(displayForecastBrussels);
-}
-
-// get coordinates for forecast function Luxemburg
-function getForecastLuxemburg(coordinates) {
-  console.log(coordinates);
-  let apiKey = "39c5b362f47e0665da2f1eefc0c7ab6b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
-  axios.get(apiUrl).then(displayForecastLuxemburg);
-}
-
-// get coordinates for forecast function Paris
-function getForecastParis(coordinates) {
-  console.log(coordinates);
-  let apiKey = "39c5b362f47e0665da2f1eefc0c7ab6b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
-  axios.get(apiUrl).then(displayForecastParis);
 }
 
 // current temperature function
@@ -244,6 +122,7 @@ function showCurrentWeather(response) {
   let wind = response.data.wind.speed;
   let humidity = response.data.main.humidity;
   let icon = response.data.weather[0].icon;
+  let city = response.data.name;
 
   let displayTemp = document.querySelector("#current-temperature");
   displayTemp.innerHTML = `${temperature}˚C`;
@@ -256,6 +135,9 @@ function showCurrentWeather(response) {
 
   let displayIcon = document.querySelector("#icon");
   displayIcon.setAttribute("src", `images/${icon}.png`);
+
+  let displayCity = document.querySelector("h1");
+  displayCity.innerHTML = city;
 
   getForecast(response.data.coord);
 }
@@ -284,159 +166,56 @@ function showCurrentWeatherCurrentLocation(response) {
   getForecastCurrentLocation(response.data.coord);
 }
 
-// current temperature function Berlin
-function showCurrentWeatherBerlin(response) {
-  celciusTemperature = response.data.main.temp;
-
-  let temperature = Math.round(celciusTemperature);
-  let wind = response.data.wind.speed;
-  let humidity = response.data.main.humidity;
-
-  let displayTemp = document.querySelector("#current-temperature");
-  displayTemp.innerHTML = `${temperature}˚C`;
-
-  let displayWind = document.querySelector("#wind");
-  displayWind.innerHTML = `Wind: ${wind}m/s`;
-
-  let displayHumidity = document.querySelector("#humidity");
-  displayHumidity.innerHTML = `Humidity: ${humidity}%`;
-
-  let displayCity = document.querySelector("h1");
-  displayCity.innerHTML = "Berlin";
-
-  getForecastBerlin(response.data.coord);
-}
-
-// current temperature function Brussels
-function showCurrentWeatherBrussels(response) {
-  celciusTemperature = response.data.main.temp;
-
-  let temperature = Math.round(celciusTemperature);
-  let wind = response.data.wind.speed;
-  let humidity = response.data.main.humidity;
-
-  let displayTemp = document.querySelector("#current-temperature");
-  displayTemp.innerHTML = `${temperature}˚C`;
-
-  let displayWind = document.querySelector("#wind");
-  displayWind.innerHTML = `Wind: ${wind}m/s`;
-
-  let displayHumidity = document.querySelector("#humidity");
-  displayHumidity.innerHTML = `Humidity: ${humidity}%`;
-
-  let displayCity = document.querySelector("h1");
-  displayCity.innerHTML = "Brussels";
-
-  getForecastBrussels(response.data.coord);
-}
-
-// current temperature function Luxemburg
-function showCurrentWeatherLuxemburg(response) {
-  celciusTemperature = response.data.main.temp;
-
-  let temperature = Math.round(celciusTemperature);
-  let wind = response.data.wind.speed;
-  let humidity = response.data.main.humidity;
-
-  let displayTemp = document.querySelector("#current-temperature");
-  displayTemp.innerHTML = `${temperature}˚C`;
-
-  let displayWind = document.querySelector("#wind");
-  displayWind.innerHTML = `Wind: ${wind}m/s`;
-
-  let displayHumidity = document.querySelector("#humidity");
-  displayHumidity.innerHTML = `Humidity: ${humidity}%`;
-
-  let displayCity = document.querySelector("h1");
-  displayCity.innerHTML = "Luxemburg";
-
-  getForecastLuxemburg(response.data.coord);
-}
-
-// current temperature function Paris
-function showCurrentWeatherParis(response) {
-  celciusTemperature = response.data.main.temp;
-
-  let temperature = Math.round(celciusTemperature);
-  let wind = response.data.wind.speed;
-  let humidity = response.data.main.humidity;
-
-  let displayTemp = document.querySelector("#current-temperature");
-  displayTemp.innerHTML = `${temperature}˚C`;
-
-  let displayWind = document.querySelector("#wind");
-  displayWind.innerHTML = `Wind: ${wind}m/s`;
-
-  let displayHumidity = document.querySelector("#humidity");
-  displayHumidity.innerHTML = `Humidity: ${humidity}%`;
-
-  let displayCity = document.querySelector("h1");
-  displayCity.innerHTML = "Paris";
-
-  getForecastParis(response.data.coord);
-}
-
-// city display function
-function enterCity(event) {
-  event.preventDefault();
-  let cityTitle = document.querySelector("#city-input");
-  let cityDisplay = document.querySelector("h1");
-  cityDisplay.innerHTML = `${cityTitle.value}`;
+function search(city) {
   let unit = "metric";
   let apiKey = "39c5b362f47e0665da2f1eefc0c7ab6b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityTitle.value}&appid=${apiKey}&units=${unit}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
   axios.get(apiUrl).then(showCurrentWeather);
 }
 
-// berlin display function
+// city display function
+function submitCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+
+//city variables
+let cityInput = document.querySelector("#city-form");
+cityInput.addEventListener("submit", submitCity);
+
+// Functions for quick links
+
+// Berlin
 function clickBerlin(event) {
   event.preventDefault();
-  let unit = "metric";
-  let apiKey = "39c5b362f47e0665da2f1eefc0c7ab6b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${apiKey}&units=${unit}`;
-  axios.get(apiUrl).then(showCurrentWeatherBerlin);
+  search("Berlin");
 }
 
-// brussels display function
-function clickBrussels(event) {
-  event.preventDefault();
-  let unit = "metric";
-  let apiKey = "39c5b362f47e0665da2f1eefc0c7ab6b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=brussels&appid=${apiKey}&units=${unit}`;
-  axios.get(apiUrl).then(showCurrentWeatherBrussels);
-}
-
-// luxemburg display function
-function clickLuxemburg(event) {
-  event.preventDefault();
-  let unit = "metric";
-  let apiKey = "39c5b362f47e0665da2f1eefc0c7ab6b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=luxemburg&appid=${apiKey}&units=${unit}`;
-  axios.get(apiUrl).then(showCurrentWeatherLuxemburg);
-}
-
-// paris display function
-function clickParis(event) {
-  event.preventDefault();
-  let unit = "metric";
-  let apiKey = "39c5b362f47e0665da2f1eefc0c7ab6b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=paris&appid=${apiKey}&units=${unit}`;
-  axios.get(apiUrl).then(showCurrentWeatherParis);
-}
-
-// click berlin link
 let berlinLink = document.querySelector("#berlin");
 berlinLink.addEventListener("click", clickBerlin);
 
-// click brussels link
+// Brussels
+function clickBrussels(event) {
+  event.preventDefault();
+  search("Brussels");
+}
 let brusselsLink = document.querySelector("#brussels");
 brusselsLink.addEventListener("click", clickBrussels);
 
-// click luxemburg link
+// Luxemburg
+function clickLuxemburg(event) {
+  event.preventDefault();
+  search("Luxemburg");
+}
 let luxemburgLink = document.querySelector("#luxemburg");
 luxemburgLink.addEventListener("click", clickLuxemburg);
 
-// click paris link
+// Paris
+function clickParis(event) {
+  event.preventDefault();
+  search("Paris");
+}
 let parisLink = document.querySelector("#paris");
 parisLink.addEventListener("click", clickParis);
 
@@ -444,10 +223,6 @@ parisLink.addEventListener("click", clickParis);
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
-
-//city variables
-let cityInput = document.querySelector("#city-form");
-cityInput.addEventListener("submit", enterCity);
 
 // current location display function
 function showPosition(location) {
@@ -469,3 +244,4 @@ let currentLocationLink = document.querySelector("#current-location");
 currentLocationLink.addEventListener("click", showCurrentPosition);
 
 let celciusTemperature = null;
+search("Brussels");
